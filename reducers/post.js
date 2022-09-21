5e46e70f3f6f36cfcd5ea0abbe62b056e2ca2cba
youngeun choi <choi46910@naver.com>
2022-09-21 17:20:43 +0900

    Add : 게시글 폼

--- reducers/post.js
+++ reducers/post.js
@@ -1,9 +1,54 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [{
+    id: 1,
+    user: {
+      id: 1,
+      nickname: 'youngeun',
+    },
+    content: '첫 번째 게시글 #해시태그',
+    Images: [{
+      src: "https://img.hankyung.com/photo/202012/01.24791736.1.jpg"
+    }],
+    Comments: [{
+      User: {
+        nickname: 'nero',
+      },
+      content: '우와~~',
+    }, {
+      User: {
+        nickname: 'hero',
+      },
+      content: '굿굿',
+    }]
+  }],
+  imagePaths: [],
+  postAdded: false,
 }
+
+const ADD_POST = 'ADD_POST';
+export const addPost = { //액션객체
+  type: ADD_POST,
+}
+
+const dummyPost = {
+  id: 2,
+  content: '더미데이터',
+  User: {
+    id: 1,
+    nickname: 'youngeun',
+  },
+  Image: [],
+  Comments: [],
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      }
     default:
       return state;
   }
